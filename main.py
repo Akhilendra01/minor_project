d6590d8ea5f78579133e6664818af27808ea8e97
Akhilendra01 <akhilendra1604@gmail.com>
2024-03-09 15:05:12 +0530

    added db connection

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
 import random
 import string
 import uuid
-
+from flask import Flask, request
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
+import mysql.connector
+
+
+def connectDB():
+    try:
+        connection = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='admin@123',
+            database='test'
+        )
+
+        if connection.is_connected():
+            print('Connected to MySQL database')
+
+        return connection
+
+    except mysql.connector.Error as e:
+        print(f"Error connecting to MySQL database: {e}")
+
+connection=connectDB()
+cursor=connection.cursor()
+
+def query(sql):
+    cursor.execute(sql)
+    return cursor.fetchall()
 
 def generate_pseudonym(length=8):
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for _ in range(length))
 
 def generate_rsa_key_pair():
-    # Generate an RSA key pair
     private_key = rsa.generate_private_key(
         public_exponent=65537,
         key_size=2048 
@@ -20,30 +45,16 @@ def generate_rsa_key_pair():
 def get_mac_address():
     mac = uuid.UUID(int=uuid.getnode()).hex[-12:]
     return ':'.join([mac[e:e+2] for e in range(0, 11, 2)])
-def main():
-    private_key = generate_rsa_key_pair()
 
-    pem = private_key.private_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PrivateFormat.PKCS8,
-        encryption_algorithm=serialization.NoEncryption()
-    )
+app=Flask(__name__)
 
-    public_key = private_key.public_key()
-    pem_public = public_key.public_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo
-    )
 
-    print("Private Key:")
-    print(pem.decode('utf-8'))
-    print("\nPublic Key:")
-    print(pem_public.decode('utf-8'))
-    random_pseudonym = generate_pseudonym()
-    print("Random pseudonym:", random_pseudonym)
 
-    print("MAC Address:", get_mac_address())
+@app.route('/')
+def hello():
+    print(query('select * from user;'))
+    return f"{request.remote_addr}"
 
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
