dd09723fa94d9eff933e60efc558a207f516c063
Akhilendra01 <akhilendra1604@gmail.com>
2024-04-07 21:16:10 +0530

    /

--- Client/main.py
+++ Client/main.py
@@ -1,6 +1,19 @@
 import socket 
 import Helpers
+import Constants
 
 ip=Helpers.get_ip_address()
-print(ip)
-# class Client:
+port=Constants.CLIENT_PORT
+
+print(ip, port)
+class Client:
+    def __init__(self):
+        pass
+
+
+
+def main():
+    client=Client()
+
+if __name__=="main":
+    main()
